0549e65eebf944d509ab7fb6d0bc49104855450a
andrav24 <andrav24@yandex.ru>
2018-09-28 20:53:03 +0700

    1.3 JavaDoc Code convention

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,15 @@
 package ru.job4j.calculate;
 
+/**
+ * Выводит фразу " Hello World!"
+ * @author Antonov Andrey
+*/
+
 public class Calculate{
-  
+
+/**
+ * Это метод main
+ */  
   public static void main(String[] args){
     
     System.out.println("Hello world!");
