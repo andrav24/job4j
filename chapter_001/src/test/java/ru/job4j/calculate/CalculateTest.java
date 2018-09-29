e581ce2681afa62b2987e71bec2876578e859584
andrav24 <andrav24@yandex.ru>
2018-09-29 18:18:55 +0700

    1.4. JUnit

---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test
+ * @author Andrey Antonov
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class CalculateTest {
+/**
+ * Test echo
+ */
+  @Test
+  public void whenTakeNameThenThreeEchoPlusName(){
+
+    String input = "Andrey Antonov";
+    String expect = "Echo, echo, echo : Andrey Antonov";
+    Calculate calc = new Calculate();
+    String result = calc.echo(input);
+    assertThat(result, is(expect));
+
+  }
+
+}
+
