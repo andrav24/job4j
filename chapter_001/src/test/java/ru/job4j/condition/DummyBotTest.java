39d5e20bb175fe27059d71e3d905efad73b40967
andrav24 <andrav24@yandex.ru>
2018-10-05 19:19:33 +0700

    4.1. Глупый бот.

---
+++ chapter_001/src/test/java/ru/job4j/condition/DummyBotTest.java
@@ -0,0 +1,50 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Тесты класса DummyBot.
+ * @author Andrey Antonov
+ * @version $Id$
+ * @since 0.1
+ */
+public class DummyBotTest {
+
+    /**
+     * тест ответа на фразу:"Привет, Бот."
+     */
+    @Test
+    public void whenGreetBot() {
+        DummyBot bot = new DummyBot();
+        assertThat(
+                bot.answer("Привет, Бот."),
+                is("Привет, умник.")
+        );
+    }
+
+    /**
+     * тест ответа на фразу:"Пока."
+     */
+    @Test
+    public void whenByuBot() {
+        DummyBot bot = new DummyBot();
+        assertThat(
+                bot.answer("Пока."),
+                is("До скорой встречи.")
+        );
+    }
+
+    /**
+     * тест ответа на фразу, на которую бот не знает как отвечать.
+     */
+    @Test
+    public void whenUnknownBot() {
+        DummyBot bot = new DummyBot();
+        assertThat(
+                bot.answer("Сколько будет 2 + 2?"),
+                is("Это ставит меня в тупик. Спросите другой вопрос.")
+        );
+    }
+}
