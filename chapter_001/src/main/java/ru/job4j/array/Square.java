c6941d68cdfad48ad5bca7dae7663f3b0e2b9363
andrav24 <andrav24@yandex.ru>
2018-10-10 05:47:49 +0700

    6.0. Заполнить массив степенями чисел.

---
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+/**
+ * Класс для работы с массивами
+ */
+public class Square {
+    /**
+     * заполняет массив через цикл элементами от 1 до bound возведенными в квадрат
+     * @param bound верхняя граница
+     * @return  массив квадратов чисел от 1 до bound
+     */
+    public int[] calculate(int bound) {
+        int[] rst = new int[bound];
+        for (int i = 0; i < bound; i++) {
+            rst[i] = (int) Math.pow(i + 1 , 2);
+        }
+        return rst;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/SquareTest.java
@@ -0,0 +1,57 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * test for class Square
+ */
+public class SquareTest {
+
+    /**
+     * тест метода calculate
+     * возвращаем новый массив, в котором каждому элементу входящего массива
+     * соответсвует его квадрат
+     * тест для 1 до 3
+     */
+    @Test
+    public void whenBound3Then1_4_9() {
+        int bound = 3;
+        Square square = new Square();
+        int[] rst = square.calculate(bound);
+        int[] expect = new int[] {1, 4, 9};
+        assertThat(rst, is(expect));
+    }
+
+    /**
+     * тест метода calculate
+     * возвращаем новый массив, в котором каждому элементу входящего массива
+     * соответсвует его квадрат
+     * тест для 1 до 5
+     */
+    @Test
+    public void whenBound5Then1_4_9_16_25() {
+        int bound = 5;
+        Square square = new Square();
+        int[] rst = square.calculate(bound);
+        int[] expect = new int[] {1, 4, 9, 16, 25};
+        assertThat(rst, is(expect));
+    }
+
+    /**
+     * тест метода calculate
+     * возвращаем новый массив, в котором каждому элементу входящего массива
+     * соответсвует его квадрат
+     * тест для 1 до 1
+     */
+    @Test
+    public void whenBound1Then1() {
+        int bound = 1;
+        Square square = new Square();
+        int[] rst = square.calculate(bound);
+        int[] expect = new int[] {1};
+        assertThat(rst, is(expect));
+    }
+}
+
